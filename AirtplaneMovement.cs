3bfa8695a7adeeb249cecc545cc6614e83bcf6a6
Ethan Barrett <ethan_barrett@live.com>
2022-11-28 15:36:59 -0500

    initial commit

---
+++ AirtplaneMovement.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AirtplaneMovement : MonoBehaviour {
+
+    public Vector2 speed = new Vector2(50, 50);
+    // Update is called once per frame
+    void Update()
+    {
+        float inputX = Input.GetAxis("Horizontal");
+        float inputY = Input.GetAxis("Vertical");
+
+        if (inputX > 0) {
+            Vector3 movement = new Vector3(speed.x * inputX, speed.y * inputY, 0);
+            movement *= Time.deltaTime;
+            transform.Translate(movement);
+        }
+
+       
+        
+
+            
+            
+
+        
+    }
+
+
+}
